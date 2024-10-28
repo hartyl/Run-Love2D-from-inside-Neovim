e6e3198785c4a8b803fcad0e7839b6060a902d17
HARTYL YHEA BOI my heart is not broken fu*k <101079305+hartyl@users.noreply.github.com>
2024-10-27 22:00:16 -0300

    Now with Python3 and C++
    Didn't test C++ last change, i'm kinda tired

--- remap.lua
+++ remap.lua
@@ -14,20 +14,36 @@ vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd(
 	[[
 	" if it finds a main.lua in the project folder
-	if execute("!ls main.lua") =~ "main\.lua\n$"
+	silent if execute("!ls main.lua") =~ "main\.lua\n$"
 		" run love in the project file
 		silent !love .
 		" If you are using WSL, add the line "export PATH=$PATH:/mnt/c/"Program Files"/LOVE" or similar to /~/.bashrc
 		" silent !love.exe .
 	" if you are currently editing a lua file, shout out it
 	elseif expand("%") =~ "\.lua$"
+		echom "Executing..."
 		so
 	" nice C++ addition! Takes the input from a text file called input.txt in the project folder's root,
 	" and outputs directly to Windows's clipboard
 	" This version implements g++ with c++20
 	elseif expand("%") =~ "\.cpp$"
 		echom "Executing..."
-		silent execute("!g++ -std=c++20 % && xclip -i input.txt && xclip -o | ./a.out | xclip -selection c && rm a.out")
-		echom "C++ Executed!"
+		silent w
+		silent let output = execute("!g++ -std=c++20 % && xclip -i input.txt && xclip -o | ./a.out | xclip -selection c && rm a.out")
+		if output =~ "ERROR"
+			echo output
+		else
+			!xclip -selection c -o
+		endif
+	" Now why not python
+	elseif expand("%") =~ "\.py$"
+		echom "Executing..."
+		silent w
+		silent let output = execute("!xclip -i input.txt && xclip -o | python3 % | xclip -selection c")
+		if output =~ "Error"
+			echo output
+		else
+			!xclip -selection c -o
+		endif
 	endif]])
 end)
